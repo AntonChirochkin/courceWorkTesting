022ece468bf8478dda4c7074f5df22e82c6e62b2
AntonChirochkin <belix888@yandex.ru>
2023-08-27 20:52:58 +0500

    Курсовая работа. Тестирование. Исходный код.

---
+++ src/main/java/ru/skypro/courseworktesting/CourseWorkTestingApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.courseworktesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkTestingApplication.class, args);
+    }
+
+}
